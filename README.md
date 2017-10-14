119f670db7fb403ea4b21329e6de6b376455182c
Gary Gu <gu.gary@gmail.com>
2017-10-13 20:24:16 -0400

    get price from heroku

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # react-table
 
-See [live demo here](http://less.azurewebsites.net/). Please use Chrome, Safari or Opera only.
-
 This little app illustrates that one can write a usefull React app using only plain html, javascript and css, without having to install anything!
 A dumb terminal plus vi would do the job. More specifically, there's no need to have a fancy IDE or
 a whole slew of things such as nested node_modules, JSX, ES6, Typescript, webpack, Sass, Babel, Browserify, Gulp or Flux.
+
+See the live demo, during NYSE trading hours, [right here](https://gugary.github.io/react-table/). Please use Chrome, Safari or Opera only.
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var mycols = [
+const mycols = [
     {name:'t', disp:'Symbol', klass:'symbol', type:'ticker'},
     {name:'e', disp:'Exchange', klass:'exchange', type:'string'},
     {name:'l', disp:'Last Price', klass:'price', type:'string'},
@@ -25,12 +25,12 @@ function mkComp(spec){
 function mkElem(tag,props,children){
     return React.createElement(tag,props,children);
 }
-var th = mkComp({
+const th = mkComp({
     render: function(){
         return mkElem('th', {className:"gth"}, this.props.val);
     }
 });
-var thead = mkComp({
+const thead = mkComp({
     render: function(){
         return mkElem('thead', {},
             mkElem("tr", {}, this.props.cols.map(function(c,j){
@@ -39,36 +39,48 @@ var thead = mkComp({
         )
     }
 });
-var td = mkComp({
+const td = mkComp({
     render: function(){
-        var c=this.props.col, v=this.props.val, r=this.props.row;
+        const c=this.props.col, v=this.props.val, r=this.props.row;
         return mkElem('td', {className:c.klass}, cellByType(c.type,v,r));
     }
 });
-var tr = mkComp({
+const tr = mkComp({
     render: function(){
-        var myrow=this.props.row;
+        const myrow=this.props.row;
         return mkElem("tr", {}, this.props.cols.map(function(c,j){
             return mkElem(td, {key:j, val:myrow[c.name], col:c, row:myrow});
         }));
     }
 });
-var tbody = mkComp({
+const tbody = mkComp({
     render: function(){
-        var mycols=this.props.cols;
+        const mycols=this.props.cols;
         return mkElem('tbody', {}, this.props.rows.map(function(r,j){
             return mkElem(tr, {key:j, row:r, cols:mycols});
         }));
     }
 });
-var nyse_holidays=["2016/11/24","2016/12/26","2017/01/02","2017/01/16","2017/02/20","2017/04/14","2017/05/29","2017/07/04","2017/09/04","2017/11/23","2017/12/25"];
-var holidays = nyse_holidays.map(function(d){
-    var p=d.split('/');
-    var d=new Date(p[0],p[1]-1,p[2]);
-    return d.toDateString();
+const nyse_holidays=[
+    "2017/11/23",
+    "2017/12/25",
+    "2018/01/01",
+    "2018/01/15",
+    "2018/02/19",
+    "2018/03/30",
+    "2018/05/28",
+    "2018/07/04",
+    "2018/09/03",
+    "2018/11/22",
+    "2018/12/25",
+];
+const holidays = nyse_holidays.map(function(d){
+    const p=d.split('/');
+    const nd=new Date(p[0],p[1]-1,p[2]);
+    return nd.toDateString();
 });
 function isWeekend(d){
-    var name = d.substr(0,3);
+    const name = d.substr(0,3);
     return name==='Sat' || name==='Sun';
 }
 function isTradingDay(d){
@@ -78,18 +90,18 @@ function isHoliday(d){
     return holidays.indexOf(d)>=0;
 }
 function isTradingHour(){
-    var d=new Date(),h=d.getUTCHours();
-	var open=isTradingDay(d.toDateString());
+    const d=new Date(),h=d.getUTCHours();
+    const open=isTradingDay(d.toDateString());
     return open && h<20 && (h>13 || (h==13 && 30<=d.getMinutes()));
 }
-var table = mkComp({
+const table = mkComp({
     getInitialState: function(){
         return {rows:[]}
     },
     onTimer: function(){
-        if(isTradingHour()){
+        //if(isTradingHour()){
             this.googleApi();
-        }
+        //}
     },
     googleApi: function(){
         $.ajax({
@@ -106,10 +118,10 @@ var table = mkComp({
     },
     componentDidMount: function() {
         this.googleApi();
-        setInterval(this.onTimer, 1000);
+        setInterval(this.onTimer, 600000);
     },
     render: function(){
-		var mycols=this.props.cols;
+	const mycols=this.props.cols;
         return mkElem('table', {},[
             mkElem(thead, {key:0, cols:mycols}),
             mkElem(tbody, {key:1, cols:mycols, rows:this.state.rows})
@@ -117,6 +129,6 @@ var table = mkComp({
     }
 });
 ReactDOM.render(
-    mkElem(table, {url:"/google_finance_api", cols:mycols}),
+    mkElem(table, {url:"https://ggu.herokuapp.com/get", cols:mycols}),
     document.getElementById('gug')
 );

bc5791cf81827afe55d63caa5958510e1bf2a23f
Gary Gu <gu.gary@gmail.com>
2019-07-04 08:16:06 -0400

    holiday

--- index.js
+++ index.js
@@ -59,11 +59,10 @@ class Tbody extends React.Component{
     }
 }
 const nyse_holidays=[
-    "2019/01/21",
-    "2019/02/18",
-    "2019/04/19",
-    "2019/05/27",
     "2019/07/04",
+    "2019/09/02",
+    "2019/11/28",
+    "2019/12/25",
 ];
 const holidays = nyse_holidays.map(function(d){
     const p=d.split('/');
